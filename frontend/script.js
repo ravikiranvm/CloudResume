6ce4026756e22f8d9ffc775f804451e20cab01c5
Ravi Kiran Vallamkonda <ravikiran@Ravis-MacBook-Air.local>
2024-12-09 18:19:41 +0530

    Testing again if Workflow is working as expected

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
     .then(data => {
         let visitor_count = data.updated_total_count;
 
-        document.querySelector('#footer').innerHTML = `This resume caught the eye of ${visitor_count} curious minds`;
+        document.querySelector('#footer').innerHTML = `This resume caught the eye of ${visitor_count} curious minds!!!!!`;
     })
     .catch(error => {
         console.log('Error:', error);
